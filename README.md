b142dc6678d83b1022cc75c2361bd82f5c50d435
timpriadko <timpriadko@gmail.com>
2024-02-21 16:38:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# prom-training-app
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/timpriadko/prom-training-app)
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,11 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+const Navigation = () => {
+  return (
+    <div className="nav-wrapper">
+      <h3>Navigation</h3>
+    </div>
+  );
+};
+
+export default Navigation;
